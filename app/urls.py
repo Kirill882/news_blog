b2373edbf99016beea126df3cd958f96f0add380
Me <NT@mail.com>
2019-09-19 19:05:47 +0300

    added path 'accounts' to app.urls

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('post/<slug:slug>/comment', views.add_comment_to_post, name='add_comment_to_post'),
-    path('tag/<str:slug>', views.tag_detail, name='tag_detail_url')
+    path('tag/<str:slug>', views.tag_detail, name='tag_detail_url'),
 ]
